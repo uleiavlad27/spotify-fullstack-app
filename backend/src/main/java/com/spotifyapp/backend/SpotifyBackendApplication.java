974ec9c4225b57f5e56812e659c25b1ca1f11123
Vlad-Ștefan Uleia <138117482+vladuleia@users.noreply.github.com>
2025-11-28 21:40:46 +0200

    Initial setup: Backend and Frontend

---
+++ backend/src/main/java/com/spotifyapp/backend/SpotifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spotifyapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyBackendApplication.class, args);
+	}
+
+}
